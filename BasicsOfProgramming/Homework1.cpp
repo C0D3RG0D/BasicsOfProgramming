206c8520a4832be2150712766c57b1bce44887e7
C0D3RG0D <82378312+C0D3RG0D@users.noreply.github.com>
2023-09-15 20:55:41 +0200

    Add files via upload

---
+++ BasicsOfProgramming/Homework1.cpp
@@ -0,0 +1,25 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+int AreaCircle()
+{
+    setlocale(LC_ALL, "RUS");
+    double R, S;
+    std::cout << "Введите радиус\n";
+    std::cin >> R;
+
+    if (R > 0 and R < 100) {
+        S = R * R * M_PI;
+        std::cout << std::fixed << std::setprecision(9) << "Площадь круга равна: " << S;
+    }
+    else {
+        std::cout << "Радиус должен находиться в диапозоне от 1 до 100!!! (0 < R < 100)";
+        return 1;
+    }
+
+    return 0;
+
+}
+
